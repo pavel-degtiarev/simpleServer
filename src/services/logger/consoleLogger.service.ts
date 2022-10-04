bbdc799a7841fc14af63f7ef4e1bc7235b40e07a
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-10-04 20:17:34 +0700

    добавлен LoggerService

---
+++ src/services/logger/consoleLogger.service.ts
@@ -0,0 +1,27 @@
+import { Logger } from "tslog";
+import { ILogger } from "./loggerInterface.js";
+
+export class ConsoleLogger implements ILogger {
+  private logger: Logger;
+
+  constructor() {
+    this.logger = new Logger({
+      displayInstanceName: false,
+      displayLoggerName: false,
+      displayFunctionName: false,
+      displayFilePath: "hideNodeModulesOnly",
+    });
+  }
+
+  log(...args: unknown[]) {
+    this.logger.info(...args);
+  }
+
+  warn(...args: unknown[]) {
+    this.logger.warn(...args);
+  }
+
+  error(...args: unknown[]) {
+    this.logger.error(...args);
+  }
+}
