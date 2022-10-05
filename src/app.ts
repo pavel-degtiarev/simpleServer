d5d64a796b73bd04b4cc9d1495f76f4472822674
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-10-05 13:59:14 +0700

    в App добавляется массив контроллеров  с роутами

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import { Express, Router } from "express";
 import { Server } from "http";
 import { ILogger } from "./common/interfaces/logger.interface.js";
-import { userRouter } from "./users/users.js";
+import { RouteController } from "./common/interfaces/routeController.interface.js";
 
 export class App {
   expressApp: Express;
   server: Server;
   port: number;
   logger: ILogger;
+  routers: RouteController[];
 
-  constructor(app: Express, logger: ILogger) {
+  constructor(app: Express, routers: RouteController[], logger: ILogger) {
     this.expressApp = app;
+    this.routers = routers;
     this.logger = logger;
   }
 
@@ -21,7 +23,7 @@ export class App {
   init(port: number) {
     this.port = port;
 
-    this.useRoute("/users", userRouter);
+    this.routers.forEach((item) => this.useRoute(item.route, item.controller.router));
 
     this.server = this.expressApp.listen(this.port, () =>
       this.logger.log(`Server started on port ${this.port}`)
