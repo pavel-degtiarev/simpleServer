8eeec73a9434a80a2dd170669543120aaafa9add
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-10-05 14:29:01 +0700

    UserRoutes перенесены в UserController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,26 @@
+import { NextFunction, Request, Response } from "express";
 import { BaseController } from "../common/base.controller.js";
 import { ILogger } from "../common/interfaces/logger.interface.js";
-import { userRoutes } from "./user.routes.js";
 
 export class UsersController extends BaseController {
   constructor(logger: ILogger) {
     super(logger);
-    this.bindRoutes(userRoutes);
+    this.bindRoutes([
+      { method: "get", endpoint: "/", handler: this.root.bind(this) },
+      { method: "post", endpoint: "/login", handler: this.login.bind(this) },
+      { method: "post", endpoint: "/register", handler: this.register.bind(this) },
+    ]);
+  }
+
+  root(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, "");
+  }
+
+  login(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, "login");
+  }
+
+  register(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, "register");
   }
 }
