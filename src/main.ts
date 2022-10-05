a8d37bbb315bd378dadf31bc6e415e7773d98354
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-10-05 13:59:51 +0700

    передаем в App массив контроллеров

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import { App } from "./app.js";
+import { RouteController } from "./common/interfaces/routeController.interface.js";
 import { ConsoleLogger } from "./services/logger/consoleLogger.service.js";
+import { UsersController } from "./users/users.controller.js";
+
+const consoleLogger = new ConsoleLogger();
+
+const routers: RouteController[] = [
+  {
+    route: "/users",
+    controller: new UsersController(consoleLogger),
+  },
+];
 
 function main() {
-  const app = new App(express(), new ConsoleLogger());
+  const app = new App(express(), routers, consoleLogger);
   app.init(8000);
 }
 
